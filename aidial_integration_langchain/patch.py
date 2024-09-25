da7f4dc94af4fbd16a5ee0b3edf184dade4bdce6
Anton Dubovik <anton_dubovik@epam.com>
2024-09-25 15:35:03 +0100

    fix: refactored patch methods

--- aidial_integration_langchain/patch.py
+++ aidial_integration_langchain/patch.py
@@ -19,18 +19,6 @@ import openai
 from langchain_core.messages import BaseMessage, BaseMessageChunk
 from langchain_core.outputs import ChatGenerationChunk
 from langchain_openai.chat_models.base import BaseChatOpenAI
-from langchain_openai.chat_models.base import (
-    _convert_chunk_to_generation_chunk as _original_convert_chunk_to_generation_chunk,
-)
-from langchain_openai.chat_models.base import (
-    _convert_delta_to_message_chunk as _original_convert_delta_to_message_chunk,
-)
-from langchain_openai.chat_models.base import (
-    _convert_dict_to_message as _original_convert_dict_to_message,
-)
-from langchain_openai.chat_models.base import (
-    _convert_message_to_dict as _original_convert_message_to_dict,
-)
 
 logging.getLogger(__name__).info("Patching langchain_open library...")
 
@@ -49,88 +37,115 @@ EXTRA_RESPONSE_FIELDS = ["statistics"]
 ##############################
 
 
-def _patched_convert_message_to_dict(message: BaseMessage) -> dict:
-    result = _original_convert_message_to_dict(message)
-    result.update(
-        _mask_by_keys(message.additional_kwargs, EXTRA_REQUEST_MESSAGE_FIELDS)
-    )
+def _patch_convert_message_to_dict(func):
+    def _func(message: BaseMessage) -> dict:
+        result = func(message)
+        result.update(
+            _mask_by_keys(
+                message.additional_kwargs, EXTRA_REQUEST_MESSAGE_FIELDS
+            )
+        )
 
-    return result
+        return result
+
+    return _func
 
 
 langchain_openai.chat_models.base._convert_message_to_dict = (
-    _patched_convert_message_to_dict
+    _patch_convert_message_to_dict(
+        langchain_openai.chat_models.base._convert_message_to_dict
+    )
 )
 
 ##############################
 
-_original_create_chat_result = BaseChatOpenAI._create_chat_result
+
+def _patch_create_chat_result(func):
+    def _func(
+        self,
+        response: Union[dict, openai.BaseModel],
+        generation_info: Optional[Dict] = None,
+    ):
+        result = func(self, response, generation_info)
+
+        _dict = (
+            response if isinstance(response, dict) else response.model_dump()
+        )
+
+        if extra := _mask_by_keys(_dict, EXTRA_RESPONSE_FIELDS):
+            result.llm_output = result.llm_output or {}
+            result.llm_output.update(extra)
+
+        return result
+
+    return _func
 
 
-def _patched_create_chat_result(
-    self,
-    response: Union[dict, openai.BaseModel],
-    generation_info: Optional[Dict] = None,
-):
-    result = _original_create_chat_result(self, response, generation_info)
-
-    _dict = response if isinstance(response, dict) else response.model_dump()
-
-    if extra := _mask_by_keys(_dict, EXTRA_RESPONSE_FIELDS):
-        result.llm_output = result.llm_output or {}
-        result.llm_output.update(extra)
-
-    return result
-
-
-BaseChatOpenAI._create_chat_result = _patched_create_chat_result
+BaseChatOpenAI._create_chat_result = _patch_create_chat_result(
+    BaseChatOpenAI._create_chat_result
+)
 
 ##############################
 
 
-def _patched_convert_dict_to_message(_dict: Mapping[str, Any]) -> BaseMessage:
-    result = _original_convert_dict_to_message(_dict)
-    result.additional_kwargs.update(_mask_by_keys(_dict, EXTRA_RESPONSE_MESSAGE_FIELDS))  # type: ignore
+def _patch_convert_dict_to_message(func):
+    def _func(_dict: Mapping[str, Any]) -> BaseMessage:
+        result = func(_dict)
+        result.additional_kwargs.update(_mask_by_keys(_dict, EXTRA_RESPONSE_MESSAGE_FIELDS))  # type: ignore
 
-    return result
+        return result
+
+    return _func
 
 
 langchain_openai.chat_models.base._convert_dict_to_message = (
-    _patched_convert_dict_to_message
+    _patch_convert_dict_to_message(
+        langchain_openai.chat_models.base._convert_dict_to_message
+    )
 )
 
 ##############################
 
 
-def _patched_convert_delta_to_message_chunk(
-    _dict: Mapping[str, Any], default_class: Type[BaseMessageChunk]
-) -> BaseMessageChunk:
-    result = _original_convert_delta_to_message_chunk(_dict, default_class)
-    result.additional_kwargs.update(_mask_by_keys(_dict, EXTRA_RESPONSE_MESSAGE_FIELDS))  # type: ignore
-    return result
+def _patch_convert_delta_to_message_chunk(func):
+    def _func(
+        _dict: Mapping[str, Any], default_class: Type[BaseMessageChunk]
+    ) -> BaseMessageChunk:
+        result = func(_dict, default_class)
+        result.additional_kwargs.update(_mask_by_keys(_dict, EXTRA_RESPONSE_MESSAGE_FIELDS))  # type: ignore
+        return result
+
+    return _func
 
 
 langchain_openai.chat_models.base._convert_delta_to_message_chunk = (
-    _patched_convert_delta_to_message_chunk
+    _patch_convert_delta_to_message_chunk(
+        langchain_openai.chat_models.base._convert_delta_to_message_chunk
+    )
 )
 
 
 ##############################
 
 
-def _patched_convert_chunk_to_generation_chunk(
-    chunk: dict, default_chunk_class: Type, base_generation_info: Optional[Dict]
-) -> Optional[ChatGenerationChunk]:
-    result = _original_convert_chunk_to_generation_chunk(
-        chunk, default_chunk_class, base_generation_info
-    )
-    if result:
-        result.message.response_metadata.update(
-            _mask_by_keys(chunk, EXTRA_RESPONSE_FIELDS)
-        )
-    return result
+def _patch_convert_chunk_to_generation_chunk(func):
+    def _func(
+        chunk: dict,
+        default_chunk_class: Type,
+        base_generation_info: Optional[Dict],
+    ) -> Optional[ChatGenerationChunk]:
+        result = func(chunk, default_chunk_class, base_generation_info)
+        if result:
+            result.message.response_metadata.update(
+                _mask_by_keys(chunk, EXTRA_RESPONSE_FIELDS)
+            )
+        return result
+
+    return _func
 
 
 langchain_openai.chat_models.base._convert_chunk_to_generation_chunk = (
-    _patched_convert_chunk_to_generation_chunk
+    _patch_convert_chunk_to_generation_chunk(
+        langchain_openai.chat_models.base._convert_chunk_to_generation_chunk
+    )
 )
