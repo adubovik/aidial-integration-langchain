257c74a36358a6e4f90dd5e85772080a19719e22
Anton Dubovik <anton_dubovik@epam.com>
2024-09-25 13:11:47 +0100

    feat: added tests for different langchain_openai versions

--- noxfile.py
+++ noxfile.py
@@ -33,8 +33,25 @@ def format(session: nox.Session):
     format_with_args(session, SRC)
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
-def test(session: nox.Session) -> None:
-    """Runs tests"""
+supported_python_versions = [
+    "3.9",
+    "3.10",
+    "3.11",
+    "3.12",
+]
+supported_lc_openai_versions = [
+    # "0.1.22",  # fail: cannot import name '_convert_chunk_to_generation_chunk' from 'langchain_openai.chat_models.base'
+    "0.1.23",
+    "0.1.24",
+    "0.1.25",
+    "0.2.0",
+]
+
+
+@nox.session(python=supported_python_versions)
+@nox.parametrize("langchain_openai", supported_lc_openai_versions)
+def test(session: nox.Session, langchain_openai: str) -> None:
+    """Runs tests for the patch"""
     session.run("poetry", "install", external=True)
-    session.run("pytest")
+    session.install(f"langchain_openai=={langchain_openai}")
+    session.run("pytest", "-k test_patch")
