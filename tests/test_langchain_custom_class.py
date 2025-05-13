1bac4173c63b04f50e543805a3484a89b16572fc
Anton Dubovik <anton_dubovik@epam.com>
2025-05-13 16:00:45 +0200

    chore: migrate from poetry to uv (#10)

--- tests/test_langchain_custom_class.py
+++ tests/test_langchain_custom_class.py
@@ -1,5 +1,3 @@
-import pytest
-
 from tests.test_cases import (
     run_test_langchain_block,
     run_test_langchain_streaming,
@@ -7,11 +5,9 @@ from tests.test_cases import (
 from tests.utils import PatchType
 
 
-@pytest.mark.asyncio
 async def test_langchain_block():
     await run_test_langchain_block(PatchType.CUSTOM_CLASS, True)
 
 
-@pytest.mark.asyncio
 async def test_langchain_streaming():
     await run_test_langchain_streaming(PatchType.CUSTOM_CLASS, True)
